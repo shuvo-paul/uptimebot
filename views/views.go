9e55b34f672e5aac6eaaf02ddf9679e9dbc47df5
Shuvo Paul <shuvopaul@yahoo.com>
2024-11-12 18:44:27 +0600

    feat: add template Execute method

--- views/views.go
+++ views/views.go
@@ -2,7 +2,11 @@ package views
 
 import (
 	"embed"
+	"net/http"
 	"text/template"
+
+	"github.com/shuvo-paul/sitemonitor/models"
+	"github.com/shuvo-paul/sitemonitor/services"
 )
 
 type Template struct {
@@ -17,4 +21,14 @@ func (t *Template) Parse(files ...string) *template.Template {
 	// Convert *Template back to embed.FS and parse
 	paths := append([]string{"layouts/base.html"}, files...)
 	return template.Must(template.ParseFS(t.fs, paths...))
+}
+
+func (t *Template) Execute(w http.ResponseWriter, r *http.Request, tmpl *template.Template, data any) error {
+	tmpl.Funcs(template.FuncMap{
+		"currentUser": func() (*models.User, bool) {
+			return services.GetUser(r.Context())
+		},
+	})
+
+	return tmpl.Execute(w, data)
 }
