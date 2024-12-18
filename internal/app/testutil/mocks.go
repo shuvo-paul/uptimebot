d9e8f49e8350a25369aacab2d11cffeaf794f9b5
Shuvo Paul <shuvopaul@yahoo.com>
2024-12-18 18:51:36 +0600

    refactor: move MockFlashStore to testutil

---
+++ internal/app/testutil/mocks.go
@@ -0,0 +1,13 @@
+package testutil
+
+// MockFlashStore is a mock implementation of flash.FlashStoreInterface
+type MockFlashStore struct {
+}
+
+func (m *MockFlashStore) SetFlash(flashID, key string, value any) {
+
+}
+
+func (m *MockFlashStore) GetFlash(flashID, key string) any {
+	return nil
+}
