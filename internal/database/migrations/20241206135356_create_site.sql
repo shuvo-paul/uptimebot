883966b5a4a5c2732d9fbf432e840c8ffab6a166
Shuvo Paul <shuvopaul@yahoo.com>
2025-01-11 11:22:45 +0600

    feat: add user_id column and foreign key constraint to sites table in migration

--- internal/database/migrations/20241206135356_create_site.sql
+++ internal/database/migrations/20241206135356_create_site.sql
@@ -1,11 +1,13 @@
 -- +migrate Up
 CREATE TABLE sites (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
     url TEXT NOT NULL,
     status VARCHAR(50),
     enabled BOOLEAN DEFAULT true,
     interval FLOAT NOT NULL,
     status_changed_at TEXT,
+    FOREIGN KEY (user_id) REFERENCES users (id)
 );
 
 -- +migrate Down
