d1eb239e4bb5f42ac6a76bb80e32db00004ee091
Shuvo Paul <shuvopaul@yahoo.com>
2025-02-07 00:32:15 +0600

    chore(migration): add DEFAULT value to create_at column in session table

--- internal/database/migrations/20241025170551_create_session.sql
+++ internal/database/migrations/20241025170551_create_session.sql
@@ -2,7 +2,7 @@
 CREATE TABLE session (
     user_id INT NOT NULL,
     token VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT (datetime('now')),
     expires_at TIMESTAMP NOT NULL,
     FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE,
     UNIQUE (token)
